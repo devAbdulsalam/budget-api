791f7d7868141553d1683b963d35ca9114587d60
ABDULSALAM MOHAMMED <ammuftau74@gmail.com>
2026-02-24 14:29:39 +0100

    improve api

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,12 +1,10 @@
 const express = require('express');
-const {
-    getUsers,
-} = require('../controllers/userController');
+const { getUsers } = require('../controllers/userController');
 const { protect } = require('../middleware/auth');
 
 const router = express.Router();
 
-router.get('/users', protect, getUsers);
-// router.get('/users', protect, getUsers);
+router.get('/', protect, getUsers);
+router.get('/users', getUsers);
 
 module.exports = router;
